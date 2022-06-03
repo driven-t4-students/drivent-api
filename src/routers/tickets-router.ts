873c69565197786e5b1280d6aeb0b6ed827c4071
Vitor <vitormuller66@gmail.com>
2022-06-03 02:41:30 -0300

    feat: initial commit

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { getTicketByUser } from '@/controllers/tickets-controller';
+import { createBooking, getTicketByUser } from '@/controllers/tickets-controller';
 
 const ticketsRouter = Router();
 
-ticketsRouter.all('/*', authenticateToken).get('/', getTicketByUser);
+ticketsRouter.all('/*', authenticateToken).get('/', getTicketByUser).post('/reservTicket', createBooking);
 
 export { ticketsRouter };
