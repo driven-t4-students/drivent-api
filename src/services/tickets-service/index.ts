1689bb0bb24508685167c09c2ac9aa1e1d8d139b
Felipe Sakuma <fycorsak@gmail.com>
2022-06-10 11:52:04 -0300

    chore: change ticket repo to return id and enrollmentId

--- src/services/tickets-service/index.ts
+++ src/services/tickets-service/index.ts
@@ -1,18 +1,15 @@
 import { notFoundError } from '@/errors';
 import ticketRepository from '@/repositories/ticket-repository';
-import { exclude } from '@/utils/prisma-utils';
 import { Ticket, TicketType } from '@prisma/client';
 import enrollmentsService from '../enrollments-service';
 
-async function getByUserId(userId: number): Promise<ticket> {
+async function getByUserId(userId: number): Promise<Ticket> {
   const enrollment = await enrollmentsService.getOneWithAddressByUserId(userId);
 
   const ticket = await ticketRepository.findByEnrollmentId(enrollment.id);
   if (!ticket) throw notFoundError();
 
-  return {
-    ...exclude(ticket, 'id', 'enrollmentId'),
-  };
+  return ticket;
 }
 
 export type ticket = Omit<Ticket, 'id' | 'enrollmentId'>;
