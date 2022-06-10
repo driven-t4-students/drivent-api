799c4fe455e626f35ea963e377f8b82379b36f2e
guoconde <guoconde@gmail.com>
2022-06-10 19:17:44 -0300

    Feat: Starting change room route

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -3,13 +3,31 @@ import { prisma } from '@/config';
 async function findAllHotels() {
   return prisma.hotel.findMany({
     include: {
-      Room: true,
+      Room: {
+        include: {
+          Bed: true,
+        },
+      },
+    },
+  });
+}
+
+async function findHotelByBedId(id: number) {
+  return prisma.hotel.findUnique({
+    where: { id },
+    include: {
+      Room: {
+        include: {
+          Bed: true,
+        },
+      },
     },
   });
 }
 
 const hotelRepository = {
   findAllHotels,
+  findHotelByBedId,
 };
 
 export default hotelRepository;
