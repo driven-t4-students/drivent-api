61b8919cf7bf3ae245087ff61c893eeadbf76996
Felipe Sakuma <fycorsak@gmail.com>
2022-06-14 12:06:06 -0300

    chore: model database for activities feature

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,4 +4,5 @@ export * from './events-service';
 export * from './enrollments-service';
 export * from './tickets-service';
 export * from './beds-service';
+// eslint-disable-next-line import/export
 export * from './rooms-service';
